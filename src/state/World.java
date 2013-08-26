ff570c39668663840de549a515a27318de444b39
Alex Campbell <campbealex2@myvuw.ac.nz>
2013-08-27 11:28:58 +1200

    Store structure reference on all tiles containing the structure.

--- src/state/World.java
+++ src/state/World.java
@@ -28,13 +28,39 @@ public class World {
 		for(int x = 0; x < 100; x++)
 			for(int y = 0; y < 100; y++) {
 				worldTile[x][y] = new Tile(generateRandomTile());
-				if(random.nextInt(5) == 0)
-					worldTile[x][y].setStructure(new Structure(x, y, 3, 3, "dark-tree"));
 			}
+
+		for(int x = 3; x < 100; x += 10)
+			for(int y = 3; y < 100; y += 10)
+				addStructure(new Structure(x, y, 3, 3, "dark-tree"));
 	}
 
 	public World(Tile[][] tiles) {
 		worldTile = tiles;
+	}
+
+	/**
+	 * Adds a structure to all tiles it overlaps and returns true.
+	 * If the structure can't be placed, returns false without changing anything.
+	 */
+	public boolean addStructure(Structure s) {
+		int x = s.getX(), y = s.getY(), w = s.getWidth(), h = s.getHeight();
+
+		if(x < 0 || y < 0 || x+w > getXSize() || y+h > getYSize())
+			return false;
+
+		// check for overlap
+		for(int X = 0; X < w; X++)
+			for(int Y = 0; Y < h; Y++)
+				if(worldTile[x][y].getStructure() != null)
+					return false; // can't have two structures on one tile
+
+		// place the structure
+		for(int X = 0; X < w; X++)
+			for(int Y = 0; Y < h; Y++)
+				worldTile[x][y].setStructure(s);
+
+		return true;
 	}
 
 	public Tile getTile(int x, int y) {
