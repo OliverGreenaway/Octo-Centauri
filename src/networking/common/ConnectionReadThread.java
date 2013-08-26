dfc6c9d17a05fb622ff28693067bd9474c6f81e3
Mate Antunovic <just.mate.antunovic@gmail.com>
2013-08-26 16:01:51 +1200

    Wrote ConnectionReadThread.run

--- src/networking/common/ConnectionReadThread.java
+++ src/networking/common/ConnectionReadThread.java
@@ -1,6 +1,31 @@
 package networking.common;
 
+import java.awt.EventQueue;
+
 public class ConnectionReadThread extends Thread {
 	private Connection from ;
-	//private static
+
+	public ConnectionReadThread(Connection from) {
+		this.from = from;
+	}
+	/**
+	 * Reads an update from the connection, then runs it, then repeats until disconnected.
+	 */
+	@Override
+	public void run() {
+		while(true) {
+			final Update update = from.getUpdate();
+			if(update == null) {
+				// disconnected
+				return;
+			}
+
+			EventQueue.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					update.doUpdate();
+				}
+			});
+		}
+	}
 }
