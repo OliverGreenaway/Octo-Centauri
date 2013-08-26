1e47de4c25fb20e1f2b825d45a95db2beb1e4b2a
Alex Campbell <campbealex2@myvuw.ac.nz>
2013-08-27 09:50:23 +1200

    Initialize world tiles randomly when world is created.

--- src/state/World.java
+++ src/state/World.java
@@ -2,12 +2,25 @@ package state;
 
 import java.awt.Image;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class World {
 	private Tile[][] worldTile;
 
+	private Random random = new Random();
+
+	public String generateRandomTile(){
+		if(random.nextInt(2)==1)
+			return "tile";
+		else
+			return "tile0";
+	}
+
 	public World(){
 		worldTile = new Tile[50][50];
+		for(int x = 0; x < 50; x++)
+			for(int y = 0; y < 50; y++)
+				worldTile[x][y] = new Tile(generateRandomTile());
 	}
 
 	public Tile getTile(int x, int y) {
