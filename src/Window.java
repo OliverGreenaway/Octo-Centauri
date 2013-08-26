351bfc97c931be707f1d67ae648381cebc33c74e
Your Name <thomas.auberson@gmail.com>
2013-08-27 10:43:46 +1200

    Added World class to window

--- src/Window.java
+++ src/Window.java
@@ -12,6 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import state.Tile;
+import state.World;
 
 
 
@@ -66,7 +67,7 @@ public class Window extends JFrame implements KeyListener, MouseListener {
 		// set up menu
 		TopMenu menu =new TopMenu();
 		setJMenuBar(menu);
-		display = new Display(map);
+		display = new Display(new World());
 
 		drawing = new JComponent() {
 			protected void paintComponent(Graphics g) {
