0dce207c797a924f729628984b33d0bb4e8e993c
Connor Findlay <connor.findlay@ecs.vuw.ac.nz>
2013-08-26 15:25:17 +1200

    Completed Isometric rendering Algorithim

--- src/Display.java
+++ src/Display.java
@@ -1,18 +1,16 @@
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.swing.*;
 
 public class Display extends JPanel{
 
 	// FIELDS
-	public final Dimension DIMENSION = new Dimension(1920,1080);
-	public final int CAMERA_X = 60, CAMERA_Y = 60;	// Camera = 60x60
-	//public Tile[][] map = new Tile[MAP_TILES_X][MAP_TILES_Y];
+	private final Dimension DIMENSION = new Dimension(1920,1080);
+	private final int VIEW_WIDTH = 30, VIEW_HEIGHT = 30;	// Camera = 60x60
 
-	public final int TILE_WIDTH = 64, TILE_HEIGHT = 32;
+	private final int MAP_TILES_X = 200, MAP_TILES_Y = 200;
+	private Tile[][] map = new Tile[MAP_TILES_X][MAP_TILES_Y];
+
+	private final int TILE_WIDTH = 64, TILE_HEIGHT = 32;
 
 
 	private Coord camera = new Coord(0,0); // ARBITRARY START POINT
@@ -23,32 +21,48 @@ public class Display extends JPanel{
 	 */
 
 	// CONSTRUCTOR
-	public Display(){
+	public Display(Tile[][] map){
 		super();
 		setPreferredSize(DIMENSION); // Necessary?
+		this.map = map;
 	}
 
 	private static final long serialVersionUID = 8274011568777903027L;
 
 	//RENDERING
-
-
 	public void paintComponent(Graphics g){
-		paintTiles(g);
+		paintMap(g);
 
 	}
 
-	private void paintTiles(Graphics g){
+	/**Paints the "view" on-screen at any one time. The algorithm goes through,
+	 * drawing the tiles from the top down, and draws them on the graphics pane.
+	 *
+	 * @param g OVERRIDEN Parameter.
+	 */
+	private void paintMap(Graphics g){
 
-		for(int x = 0; x<CAMERA_X; x++){
-			for(int y = 0; y<CAMERA_Y; y++){
-				g.drawImage(t.getImage(),)
+		for(int x = 0; x<VIEW_WIDTH; x++){
+			for(int y = 0; y<VIEW_HEIGHT; y++){
+				Tile t = map[x+camera.x][y+camera.y];
+
+				/*This is the "magic line" -- It calculates the position of the
+				 * tile on screen, and was a slightly tricky piece of trigonometry.
+				 *
+				 * DON'T CHANGE IT UNLESS YOU *REALLY* KNOW WHAT YOU'RE DOING.
+				 * Bask in it's majesty and awe-inspiring splendour.
+				 */
+				g.drawImage(t.getImage(), (this.getWidth()/2)-(TILE_WIDTH/2) + (x-y) * (TILE_WIDTH/2), (x+y) * (TILE_HEIGHT/ 2), TILE_WIDTH, t.getImage().getHeight(null), null);
 			}
 		}
 	}
 
 	public int[] getCameraCoordinates(){
 		return new int[]{camera.x,camera.y};
+	}
+
+	public void setCameraCoordinates(int[] coord){
+		camera = new Coord(coord[0],coord[1]);
 	}
 
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,10 +1,15 @@
 import java.awt.Image;
+import javax.swing.ImageIcon;
 
 
 public class Tile {
 
 	// FIELDS
 	private Image img;
+
+	public Tile(){
+		img = new ImageIcon("resources/tile0.png").getImage();
+	}
 
 
 	public Image getImage(){
