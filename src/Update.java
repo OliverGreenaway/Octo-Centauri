ff7db96ec035ab2c8aa454f2ca7ddd5a1df5dc2c
Chris Gibb <chrisrgibb@gmail.com>
2013-08-27 15:15:21 +1200

    Finished mouse clicking and refactoring and commenting in window.java

--- src/Update.java
+++ src/Update.java
@@ -1,23 +1,24 @@
 import state.World;
 
-
 public class Update extends Thread {
-private final Display display;
-private final World world;
+	private final Display display;
+	private final World world;
+
 	public Update(World w, Display d) {
-    world = w;
-    display = d;
-	}
-public void run(){
-	while(true){
-	world.update();
-	display.repaint();
-	try {
-		Thread.sleep(100);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
+		world = w;
+		display = d;
 	}
 
-}
-}
+	public void run() {
+		while (true) {
+			world.update();
+			display.repaint();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
 }
