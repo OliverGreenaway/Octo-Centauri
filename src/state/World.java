7068f92fa9c2f3decc0cbcf80a4fc74a5c3ab102
Alex Campbell <campbealex2@myvuw.ac.nz>
2013-08-27 14:18:51 +1200

    did stuff

--- src/state/World.java
+++ src/state/World.java
@@ -40,12 +40,12 @@ public class World {
 					addStructure(new Structure(x, y, 3, 3, "Assets/Environment Objects/dark-tree.png"));
 				}
 			}
-
 		addDude(new Dude(this, 7, 7, 1, 1, "Assets/Man.png"));
 	}
 
 	public World(Tile[][] tiles) {
 		worldTile = tiles;
+		addDude(new Dude(this, 7, 7, 1, 1, "Assets/Man.png"));
 	}
 
 	/**
