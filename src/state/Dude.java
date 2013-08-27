5914d41e81b7ed647f2b43e1342e6354ffc74d7b
Alex Campbell <campbealex2@myvuw.ac.nz>
2013-08-27 15:15:52 +1200

    More comments

--- src/state/Dude.java
+++ src/state/Dude.java
@@ -6,6 +6,9 @@ import java.util.Random;
 
 import javax.swing.ImageIcon;
 
+/**
+ * Stores information about a dude.
+ */
 public class Dude {
 	/**
 	 * The coordinates of the tile under the bottom corner of the dude.
@@ -21,18 +24,56 @@ public class Dude {
 	private int width, height;
 
 	/**
-	 * The structure's image.
+	 * The dude's image.
 	 */
 	private Image image;
 
 	private World world;
 
-	public int getX() {return x;}
-	public int getY() {return y;}
-	public int getWidth() {return width;}
-	public int getHeight() {return height;}
-	public Image getImage() {return image;}
+	/**
+	 * Returns the X coordinate of the bottom corner of the dude.
+	 */
+	public int getX() {
+		return x;
+	}
 
+	/**
+	 * Returns the Y coordinate of the bottom corner of the dude.
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Returns the width of the dude, in tiles.
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+	/**
+	 * Returns the height of the dude, in tiles.
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * Returns the dude's image.
+	 */
+	public Image getImage() {
+		return image;
+	}
+
+	/**
+	 * Creates a dude.
+	 * @param world The world the dude is in.
+	 * @param x The X coordinate of the bottom corner of the dude.
+	 * @param y The Y coordinate of the bottom corner of the dude.
+	 * @param width The width of the dude.
+	 * @param height The height of the dude.
+	 * @param image The path to the dude's image.
+	 */
 	public Dude(World world, int x, int y, int width, int height, String image) {
 		this.x = x;
 		this.y = y;
@@ -77,6 +118,9 @@ public class Dude {
 		return true;
 	}
 
+	/**
+	 * Called every tick. Does stuff.
+	 */
 	public void update() {
 		// move randomly
 		int r = new Random().nextInt(4);
@@ -86,6 +130,13 @@ public class Dude {
 		if(r == 3) move(x, y-1);
 	}
 
+	/**
+	 * Draws the dude.
+	 * @param g The Graphics object to draw on.
+	 * @param width The width of the display.
+	 * @param camx The camera X.
+	 * @param camy The camera Y.
+	 */
 	public void draw(Graphics g, int width, int camx, int camy){
 		int x = this.x - camx;
 		int y = this.y - camy;
