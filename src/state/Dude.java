628deb630b368ff7026d87f53858427c76253cc7
OliverGreenaway <crackshotnz@gmail.com>
2013-08-30 11:55:02 +1200

    Changed Alien spawning and added bias to resources that can be consumed

--- src/state/Dude.java
+++ src/state/Dude.java
@@ -288,7 +288,7 @@ public class Dude implements Serializable {
 
 
 			//TODO Squids cant build so fix that instanceof dude
-			if(task == null && this instanceof Dude){
+			if(task == null && !(this instanceof Octodude) && !(this instanceof Slugdude) ){
 				task = world.tasks.poll();
 			}
 
@@ -387,7 +387,7 @@ public class Dude implements Serializable {
 
 		} else {
 			//SlugBalancing check
-			if(this instanceof Octodude && !world.isSlugBalancingEnabled()){
+			if((this instanceof Octodude || this instanceof Slugdude) && !world.isSlugBalancingEnabled()){
 				return;
 			}
 			Resource nowHarvesting = world.getNearestResource(
@@ -400,7 +400,7 @@ public class Dude implements Serializable {
 				boolean moved = followPath(nowHarvesting.getX(),
 						nowHarvesting.getY());
 				if (!moved) {
-					if (harvesting.getX() == x && harvesting.getY() == y) {
+					if (harvesting.getX() == x && harvesting.getY()      == y) {
 						harvest(harvesting);
 						harvesting = null;
 					}
