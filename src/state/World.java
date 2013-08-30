8cb6e5372cfd9ef63ea5cec9ef01fad2ccf07c8c
Chris Gibb <chrisrgibb@gmail.com>
2013-08-30 15:39:06 +1200

    added new Main planet image

--- src/state/World.java
+++ src/state/World.java
@@ -232,10 +232,7 @@ public class World {
 	 * returns false without changing anything.
 	 */
 	public boolean addDude(Dude s) {
-		System.out.println(allDudes.size());
-		if(allDudes.size()>25){
-			return false;
-		}
+
 
 		int x = s.getX(), y = s.getY(), w = s.getWidth(), h = s.getHeight();
 
